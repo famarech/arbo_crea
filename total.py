a26fa8daf1aa5748e2b42f7243eb45b2d8c893f3
famarech <tata@yahoo.com>
2022-06-01 13:38:49 +0200

    V3.1 insertion du modele et creation et copie fonctionne

--- total.py
+++ total.py
@@ -27,8 +27,11 @@ global nom_chantier_onglet2
 def main():
     fenetre = tkinter.Tk()
     fenetre.geometry("800x300")
-    icone = abspath('./ressources/logo.ico')
-    fenetre.iconbitmap(icone)
+    icone = abspath('./Ressources/logo.ico')
+    try:
+        fenetre.iconbitmap(icone)
+    except:
+        pass
     fenetre.title("ArboCréa v3.1" + "                                           " +\
                                             "Créez simplement des arborescences " +\
                                     "personnalisés pour la gestion de vos affaires")
@@ -64,6 +67,7 @@ def onglet2():
 
     lb_chemin_source_onglet2 = tkinter.Label(ecran_gauche, text="Chercher la source du modèle : ")
     chemin_acces_source_onglet2 = tkinter.StringVar()
+    chemin_acces_source_onglet2.set(abspath('./Ressources/Modele/'))
     saisie_chemin_source_onglet2 = tkinter.Entry(ecran_gauche, textvariable=chemin_acces_source_onglet2)
     btn_chemin_source_onglet2 = tkinter.Button(ecran_gauche, text="Rechercher", command=explorateur_source_onglet2)
 
@@ -88,26 +92,11 @@ def onglet2():
                                     text="Saisir les informations du chantier",
                                     width=300, height=200, bd=2, padx=50, pady=50)
 
-    lb_annee_onglet2 = tkinter.Label(ecran_droite, text="Année en 2 chiffres : ")
-    annee_onglet2 = tkinter.IntVar()
-    annee_onglet2.trace("w", create_manu_modele)
-    saisie_annee_onglet2 = tkinter.Entry(ecran_droite, textvariable=annee_onglet2)
-    saisie_annee_onglet2.insert(0, "9")
-    lb_num_onglet2 = tkinter.Label(ecran_droite, text="Numéro de l'affaire en 3 chiffres : ")
-    num_chantier_onglet2 = tkinter.IntVar()
-    num_chantier_onglet2.trace("w", create_manu_modele)
-    saisie_num_onglet2 = tkinter.Entry(ecran_droite, textvariable=num_chantier_onglet2)
-    saisie_num_onglet2.insert(0, "66")
     lb_nom_onglet2 = tkinter.Label(ecran_droite, text="Nom du chantier : ")
     nom_chantier_onglet2 = tkinter.StringVar()
-    nom_chantier_onglet2.trace("w", create_manu_modele)
     saisie_nom_onglet2 = tkinter.Entry(ecran_droite, textvariable=nom_chantier_onglet2)
-    saisie_nom_onglet2.insert(0, "Eiffel")
+    saisie_nom_onglet2.insert(0, "99 666 Eiffel")
 
-    lb_annee_onglet2.grid(row=0, column=0)
-    saisie_annee_onglet2.grid(row=0, column=1)
-    lb_num_onglet2.grid(row=1, column=0)
-    saisie_num_onglet2.grid(row=1, column=1)
     lb_nom_onglet2.grid(row=2, column=0)
     saisie_nom_onglet2.grid(row=2, column=1)
     ecran_droite.grid(row=0, column=1)
@@ -132,8 +121,6 @@ def explorateur_source_onglet2():
     chemin = filedialog.askdirectory()
     chemin_acces_source_onglet2.set(chemin)
     src_onglet2 = chemin
-    src_onglet2 = src_onglet2.replace("/", "\\\\")
-
 
 def explorateur_dest_onglet2():
     global chemin_acces_dest_onglet2
@@ -141,29 +128,14 @@ def explorateur_dest_onglet2():
     chemin = filedialog.askdirectory()
     chemin_acces_dest_onglet2.set(chemin)
     dest_onglet2 = chemin
-    dest_onglet2 = dest_onglet2.replace("/", "\\\\")
 
 def create_manu_modele():
-    global annee_onglet2
-    global num_chantier_onglet2
-    global nom_chantier_onglet2
-    global chemin_acces_source_onglet2
-    global chemin_acces_dest_onglet2
-
-    print(annee_onglet2.get())
-    print(num_chantier_onglet2.get())
-    print(nom_chantier_onglet2.get())
-
 
     path_modele = chemin_acces_source_onglet2.get()
     path_copie = chemin_acces_dest_onglet2.get()
 
-    print(path_modele)
-    print(path_copie)
-
     prototype = '00 999 Abcdef'
-    chantier = str(annee_onglet2.get()) + str(num_chantier_onglet2.get()) + nom_chantier_onglet2.get()
-
+    chantier = nom_chantier_onglet2.get()
 
     for root, dirs, files in walk(path_modele):
 
@@ -178,7 +150,6 @@ def create_manu_modele():
             filename_dst = filename_src.replace(path_modele, path_copie)
             filename_dst = filename_dst.replace(prototype, chantier)
             copyfile(filename_src, filename_dst)
-
 
 
 
